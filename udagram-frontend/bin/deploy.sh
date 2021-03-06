0fce9ffef1f2d8ddbba00791cbfce27356eeb9ed
Gerrard Ezeugwa <ezeugwagerrard@gmail.com>
2021-03-06 02:03:17 +0100

    Update config

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,3 +1,2 @@
 aws s3 cp --recursive --acl public-read ./www s3://udagram-s3/udagram-frontend/
-aws s3 mv s3://udagram-s3/udagram-frontend/ s3://udagram-s3 --recursive
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-s3/
